8b481dc4dc35166f8f21b7d4c9e339f4ff9f183b
Sárkány Marcell <13677957+sourcell@users.noreply.github.com>
2019-11-15 23:33:58 +0100

    Add files via upload

--- feleves/Program.cs
+++ feleves/Program.cs
@@ -8,41 +8,20 @@ namespace feleves
     {
         static void Main(string[] args)
         {
-            StreamReader file = new StreamReader("UTAZO.BE");
-            string[] content = file.ReadToEnd().Split("\n");
-            file.Close();
+            // reading input data
+            string[] dataset = File.ReadAllLines("UTAZO.BE");
 
-            // utazo 1
-            Traveler traveler_1 = new Traveler(int.Parse(content[0]));
-            for (int i = 1; i <= traveler_1.tripNum; i++)
-            {
-                string[] data = content[i].Split(' ');
-                int from = int.Parse(data[0]);
-                int to = int.Parse(data[1]);
-                string city = data[2];
 
-                traveler_1.trips[i-1] = new Trip(from, to, city);
-            }
+            Traveler traveler_1 = new Traveler(dataset, true);
+            Traveler traveler_2 = new Traveler(dataset, false);
 
-            // utazo 2
-            Traveler traveler_2 = new Traveler(int.Parse(content[traveler_1.tripNum+1]));
-            for (int i = traveler_1.tripNum + 2; i < traveler_1.tripNum + traveler_2.tripNum + 2; i++)
-            {
-                string[] data = content[i].Split(' ');
-                int from = int.Parse(data[0]);
-                int to = int.Parse(data[1]);
-                string city = data[2];
-
-                traveler_2.trips[i - (traveler_1.tripNum+2)] = new Trip(from, to, city);
-            }
 
             // find overlaps
             string output = traveler_1.FindOverlaps(traveler_2);
-            output = traveler_1.NumLines(output) + "\n" + output;
+            
 
-            StreamWriter kimenet = new StreamWriter("UTAZO.KI");
-            kimenet.Write(output.Trim());
-            kimenet.Close();
+            // writing output data
+            File.WriteAllText("UTAZO.KI", output.Trim());
         }
     }
 }
--- feleves/Traveler.cs
+++ feleves/Traveler.cs
@@ -7,15 +7,38 @@ namespace feleves
         public int tripNum;
         public Trip[] trips;
 
-        public Traveler(int tripNum)
+        public Traveler(string[] dataset, bool isFirst)
         {
-            this.tripNum = tripNum;
+            int i;
+            if (isFirst)
+            {
+                i = 0;
+            }
+            else
+            {
+                i = int.Parse(dataset[0]) + 1;
+            }
+
+            this.tripNum = int.Parse(dataset[i++]);
             this.trips = new Trip[tripNum];
+
+            int j = 0;
+            while (i < dataset.Length && dataset[i].Length > 1)
+            {
+                string[] data = dataset[i].Split(' ');
+                int from = int.Parse(data[0]);
+                int to = int.Parse(data[1]);
+                string city = data[2];
+
+                trips[j++] = new Trip(from, to, city);
+                i++;
+            }
         }
 
         public string FindOverlaps(Traveler other)
         {
             string output = "";
+            int overlaps = 0;
 
             for (int i = 0; i < trips.Length; i++)
             {
@@ -25,20 +48,17 @@ namespace feleves
                     if (trips[i].city == other.trips[j].city)
                     {
                         if ((other.trips[j].from >= trips[i].from && other.trips[j].from <= trips[i].to)
-                        || (other.trips[j].to >= trips[i].from && other.trips[j].to <= trips[i].to))
+                        || (other.trips[j].to >= trips[i].from && other.trips[j].to <= trips[i].to)
+                        || (other.trips[j].from <= trips[i].from && other.trips[j].to >= trips[i].to))
                         {
-                            output += city + "\n";
+                            output += city + Environment.NewLine;
+                            overlaps++;
                         }
                     }
                 }
             }
 
-            return output;
-        }
-    
-        public int NumLines(string lines)
-        {
-            return lines.Split('\n').Length - 1;
+            return overlaps + Environment.NewLine + output;
         }
     }
 }
